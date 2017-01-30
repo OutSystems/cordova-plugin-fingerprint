0afa8b6730dc59e8ddb7288ed9516adcefdf5753
luissilvaos <luis.silva@outsystems.com>
2017-01-30 17:41:18 +0000

    Fixed callback for devices without fingerprint support that are triggering the success callback

--- www/FingerPrint.js
+++ www/FingerPrint.js
@@ -7,12 +7,29 @@ var FingerPrint = function () {};
 // method to check is the touch id option is available in the device
 FingerPrint.prototype.isAvailable = function (successCallback, errorCallback) {
   // success callback function for android 
-  var successCbFingerPrintAuth = function(result) {successCallback(null);};
+  var successCbFingerPrintAuth = function(result) {
+    if(result && !result.isAvailable){
+      var message;
+      // if not support touch id
+      if(!result.isHardwareDetected){
+        message = "This device didn't support Touch ID";
+      }
+      // if not have any finger print enrolled in the device
+      else if(!result.hasEnrolledFingerprints){
+        message = "No fingers are enrolled with Touch ID";
+      }
+      errorCallback(message);
+    }
+    else{
+      successCallback(result);
+    }
+    
+  };
 
   // error callback function for android 
   var errorCbFingerPrintAuth = function(error) {
     var message;
-    if(!error.isAvailable){
+    if(error && !error.isAvailable){
       // if not support touch id
       if(!error.isHardwareDetected){
         message = "This device didn't support Touch ID";
